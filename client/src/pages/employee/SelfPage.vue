a72a5e7acb09fbb6e1a2f47d1a7d178d27595302
chaosannals <chaosannals@outlook.com>
2023-12-01 11:30:57 +0800

    save

---
+++ client/src/pages/employee/SelfPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <PageLayout class="employee-self-page">
+
+    </PageLayout>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+.employee-self-page {
+    
+}
+</style>
