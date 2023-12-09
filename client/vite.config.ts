21b10b7f28164e5113eed63dfa926fd229aa6c38
chaosannals <chaosannals@outlook.com>
2023-12-09 11:00:15 +0800

    save

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import AutoImport from 'unplugin-auto-import/vite';
-import Components from 'unplugin-vue-components/vite';
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => {
@@ -13,11 +13,7 @@ export default defineConfig(async () => {
         resolvers: [ElementPlusResolver()],
       }),
       Components({
-        dirs: [
-          'src/dialogs',
-          'src/components',
-          'src/layouts',
-        ],
+        dirs: ["src/dialogs", "src/components", "src/layouts"],
         resolvers: [ElementPlusResolver()],
       }),
     ],
@@ -27,5 +23,14 @@ export default defineConfig(async () => {
       strictPort: true,
     },
     envPrefix: ["VITE_", "TAURI_"],
+    build: {
+      // Windows：使用 Chromium ； macOS 使用 WebKit
+      target:
+        process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+      // // don't minify for debug builds
+      // minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+      // // 为调试构建生成源代码映射 (sourcemap)
+      sourcemap: !!process.env.TAURI_DEBUG,
+    },
   };
 });
--- readme.md
+++ readme.md
@@ -10,6 +10,11 @@ npm run tauri dev
 
 # 打包
 npm run tauri build
+
+# 打包调试 任一，TODO 组合有点问题。
+npm run tauri build -- --debug
+npm run build:debug
+cargo tauri build --debug
 ```
 
 ## 服务端
